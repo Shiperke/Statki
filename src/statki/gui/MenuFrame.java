03553fd5aab930a31de6d0f98e352c89d7630039
Shiperke <105072265+Shiperke@users.noreply.github.com>
2022-05-10 22:46:09 +0200

    Update MenuFrame.java

--- src/statki/gui/MenuFrame.java
+++ src/statki/gui/MenuFrame.java
@@ -10,18 +10,27 @@ import java.awt.GraphicsConfiguration;
 import java.awt.GridLayout;
 import java.awt.HeadlessException;
 import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 import javax.imageio.ImageIO;
+import javax.print.attribute.standard.Media;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -39,10 +48,11 @@ public class MenuFrame extends JFrame {
 	Color menuColor, titleColor;
 	
 	
+	
 
 	public MenuFrame() throws HeadlessException, IOException {
 		
-		this.setSize(800, 600);
+		this.setSize(650, 600);
         this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         
         
@@ -51,25 +61,27 @@ public class MenuFrame extends JFrame {
         shipPanel = new JPanel();
         
         
+        
         titlePanel.setLayout(new GridLayout(2,1));
         menuPanel.setLayout(new GridLayout(7,1));
         
         
-    
-   
-        BufferedImage img = null;
-        try {
-
-        img = ImageIO.read(new File("image/stateczek.jpg"));
+        
+        
       
-        Image newImage = img.getScaledInstance(640, 490, Image.SCALE_SMOOTH);
+     
+        ImageIcon icon = new ImageIcon("zdjecia/stateczek1.png"); //za mała ikona, w internecie nie ma za bardzo rozwiązań
+        this.setIconImage(icon.getImage());
 
-        JLabel label = new JLabel(new ImageIcon(newImage));
-        shipPanel.add(label);
-        }
-        catch (IOException e) {
-        	
-        }
+    
+  
+        ImageIcon iconu = new ImageIcon("zdjecia/statek.gif");
+		JLabel label = new JLabel();
+		label.setIcon(iconu);
+	
+      // Image newImage = img.getScaledInstance(640, 490, Image.SCALE_SMOOTH);
+     
+		shipPanel.add(label);
 
         menuColor = new Color(224, 224, 224);
         titleColor = new Color(169, 169, 169);
@@ -87,6 +99,32 @@ public class MenuFrame extends JFrame {
         
         buttonAi = new JButton("Przeciwko AI");
         buttonVs = new JButton("Przeciwko graczowi");
+        
+        buttonAi.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				GameFrame gameframe = new GameFrame();
+				gameframe.setLocationRelativeTo(null);
+				gameframe.setVisible(true);
+				setVisible(false);
+			}
+		});
+        
+        buttonVs.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				GameFrame gameframe = new GameFrame();
+				gameframe.setLocationRelativeTo(null);
+				gameframe.setVisible(true);
+				setVisible(false);
+			}
+		});
+       
+        
+        
+        
         
         title = new JLabel("Gra w Statki", SwingConstants.CENTER);
         title.setFont(new Font("Serif", Font.PLAIN, 28));
@@ -112,11 +150,12 @@ public class MenuFrame extends JFrame {
         this.add(shipPanel, BorderLayout.WEST);
         
 		
-	}
-	
+	}	
 	
 	public static void main(String[] args) throws HeadlessException, IOException {
 		MenuFrame menuFrame = new MenuFrame();
+		menuFrame.setLocationRelativeTo(null);
+		menuFrame.setResizable(false);
 		menuFrame.setVisible(true);
 	}
 		
